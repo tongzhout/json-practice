7471c5a0223fbe227143d9d726e53f3070ef5e01
tongzhout z <tongzhou20030115@gmail.com>
2024-04-16 20:49:16 +0000

    modify json-bash

--- json-practice/lab8/json-bash.sh
+++ json-practice/lab8/json-bash.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl https://aviationweather.gov/api/data/metar?ids=KMCI&format=json&taf=false&hours=12&bbox=40%2C-90%2C45%2C-85 | jq -r '.[].receiptTime' | head -n 6
+curl -s "https://aviationweather.gov/api/data/metar?ids=KMCI&format=json&taf=false&hours=12&bbox=40%2C-90%2C45%2C-85" | jq -r '.[].receiptTime' | head -n 6
